85b076d33000824df8c2274099ca0ee61dcc9680
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-08-04 21:15:48 +0700

    fix share button, card share

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const shareButton = document.getElementById("share-button");
 
 const authorSection = document.querySelector(".card-footer");
 const shareSection = document.querySelector(".card-share");
+const computedStyle = window.getComputedStyle(shareSection);
 
 shareButton.addEventListener("click", function () {
-  authorSection.style.display = "none";
-  shareSection.style.display = "flex";
-});
-
-const activeShareButton = document.getElementById("button-active");
-
-activeShareButton.addEventListener("click", function () {
-  shareSection.style.display = "none";
-  authorSection.style.display = "flex";
+  if (computedStyle.display === "none") {
+    console.log(shareSection.style);
+    shareSection.style.display = "flex";
+  } else {
+    console.log(true);
+    shareSection.style.display = "none";
+  }
 });
