20f241a981e169debcb0a606dea0f4a13cdced75
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-08-04 23:06:32 +0700

    fix button color and add animation

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const shareButton = document.getElementById("share-button");
 const authorSection = document.querySelector(".card-footer");
 const shareSection = document.querySelector(".card-share");
 const computedStyle = window.getComputedStyle(shareSection);
+const path = document.querySelector(".share svg path");
 
 shareButton.addEventListener("click", function () {
   if (computedStyle.display === "none") {
-    console.log(shareSection.style);
-    shareSection.style.display = "flex";
+    shareSection.style.display = "flex"; // show card share
+    shareButton.classList.add("share-active"); // change button background color
+    path.setAttribute("fill", "white");
+
+    shareSection.classList.add("show"); // add animation
   } else {
-    console.log(true);
-    shareSection.style.display = "none";
+    shareSection.style.display = "none"; // hide card share
+    shareButton.classList.remove("share-active");
+    path.setAttribute("fill", "#6E8098");
+
+    shareSection.classList.remove("show")
   }
 });
